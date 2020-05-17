4d84d18fdca0f3d82e324bbaa16a4959923626e1
akshayr20 <akshay.rajput5394@gmail.com>
2020-05-17 20:13:49 +0530

    feat: hosted application on firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WithDragCdkModule } from './components/with-drag-cdk/with-drag-cdk.module';
 import { WithoutDragCdkModule } from './components/without-drag-cdk/without-drag-cdk.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, WithDragCdkModule, WithoutDragCdkModule],
+  imports: [BrowserModule, WithDragCdkModule, WithoutDragCdkModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
